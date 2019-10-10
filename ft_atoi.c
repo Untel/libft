e33ec33963fb58fb2721418216b9d36bd7ba37c3
Adrien DA SILVA FERNANDES <adda-sil@e2r10p2.42.fr>
2019-10-10 17:31:15 +0200

    fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,13 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 17:37:30 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/10 17:15:21 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/10 17:31:03 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	static is_space(char c)
+#include "libft.h"
+
+int static is_space(char c)
 {
 	return (c == '\t'
 			|| c == '\n'
@@ -20,31 +22,21 @@ int	static is_space(char c)
 			|| c == ' ');
 }
 
-int	is_num(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-int	is_sign(char c)
-{
-	return (c == '-' || c == '+');
-}
-
 int	ft_atoi(char *str)
 {
 	unsigned int	i;
-	unsigned int	sign;
 	unsigned int	count;
+	int				sign;
 
 	i = 0;
 	count = 0;
 	sign = 1;
 	while (is_space(str[i]))
 		i++;
-	if (is_sign(str[i]))
+	if (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
 			sign = -1;
-	while (ft_is_digit(str[i]))
+	while (ft_isdigit(str[i]))
 		count = count * 10 + (str[i++] - '0');
 	return (count * sign);
 }
