6d82c60b8768ce3512b870e93f68a62045867c7b
Adrien DA SILVA FERNANDES <adda-sil@e2r10p2.42.fr>
2019-10-10 18:16:47 +0200

    fix

--- srcs/ft_lstclear_bonus.c
+++ srcs/ft_lstclear_bonus.c
@@ -6,21 +6,21 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 16:32:25 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/10 17:54:38 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/10 18:16:22 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list *next;
 
-	while (lst[0])
+	while (*lst)
 	{
 		next = lst[0]->next;
-		ft_lstdelone(lst[0], del);
-		lst[0] = next;
+		ft_lstdelone(*lst, del);
+		*lst = next;
 	}
+	lst = NULL;
 }
