ccffe88207d011ea6f02421fa22eb46d2b1fcb3b
Adrien DA SILVA FERNANDES <adda-sil@e2r10p4.42.fr>
2019-10-21 19:01:13 +0200

    END END

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 11:45:14 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/21 18:55:12 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/21 19:00:29 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,10 +69,10 @@ char			**ft_split_charset(char *str, char *charset);
 char			**ft_split_spaces(char *str);
 t_list			*ft_lstnew(void *content);
 t_list			*ft_lstlast(t_list *lst);
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*d)(void *));
 int				ft_lstsize(t_list *lst);
-void			ft_lstadd_front(t_list **alst, t_list *new);
-void			ft_lstadd_back(t_list **alst, t_list *new);
+void			ft_lstadd_front(t_list **alst, t_list *n);
+void			ft_lstadd_back(t_list **alst, t_list *n);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstprint(t_list *lst, char *cmt);
