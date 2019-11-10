f5322e093964623ffa2da62618e7f920891db196
Untel <adda-sil@student.42.fr>
2019-11-10 05:39:49 +0100

    Revert ft_pow to iterative

--- ft_pow.c
+++ ft_pow.c
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 10:06:17 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/11/08 16:42:57 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/11/09 01:45:35 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 uint64_t	ft_pow(uint64_t nb, int power)
 {
-	if (power > 0)
-		return (nb * ft_pow(nb, power - 1));
-	else if (power == 0)
-		return (1);
-	else
+	int			i;
+	uint64_t	count;
+
+	count = nb;
+	if (power < 0)
 		return (0);
+	if (power == 0)
+		return (1);
+	if (nb == 0)
+		return (power == 0 ? 1 : 0);
+	i = 0;
+	while (++i < power)
+		count *= nb;
+	return (count);
 }
